3fce2ba362cb4e19418707c5771227291f40190d
Danny Fang Yan <OHaiDanny>
2014-06-27 17:38:33 -0700

    Ignite One Pager Check in
    Includes mobile and desktop web version

---
+++ js/ignite.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+    $('#logo a').css('display', 'none');
+    
+    // article height fix
+    $('#about article').css('height', $('#about').height());
+    $('#contact article').css('height', $('#contact').height());
+    
+    // scroll animation when user clicks on header
+    $('a[href^="#"]').click(function () {
+        $('html, body').animate({
+            scrollTop:  $(this.hash).offset().top
+        }, 400);
+        return false;
+        e.preventDefault();
+    });
+});
+
+$(window).scroll(function() {
+   if ($(this).scrollTop() >= 300) {
+       $('#logo a').slideDown(400);
+   } else {
+       $('#logo a').slideUp(400);
+   }
+});
